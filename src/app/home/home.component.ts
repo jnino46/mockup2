80dbbd74f8de7b525c094e40e40360e69bafd338
JAIRO ADOLFO NI�O MAYORGA <jaironino229822@correo.itm.edu.co>
2019-09-29 01:32:41 -0500

    ok mockup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public titulo:string;
   constructor() {
-    this.titulo = "Listado de viajes"
+    this.titulo = "Smart Filters"
    }
 
   ngOnInit() {
